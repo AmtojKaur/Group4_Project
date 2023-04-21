ae24716ccda96ecbeb3d97f25f00cc03a9c516d9
amtojkaur <amtojk@uw.edu>
2023-04-21 14:31:09 -0700

    inital commit

---
+++ app/src/main/java/edu/uw/tcss450/group4/weatherchatapp/MainActivity.java
@@ -0,0 +1,14 @@
+package edu.uw.tcss450.group4.weatherchatapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
