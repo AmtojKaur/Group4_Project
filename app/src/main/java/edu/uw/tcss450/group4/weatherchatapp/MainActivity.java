5e037943d8d7e022e8993279d7a971f092282227
Andrew Nguyen <andrewnguyen841@gmail.com>
2023-04-26 16:00:51 -0700

    add options menu with items "edit profile," "settings," and "log out"

--- app/src/main/java/edu/uw/tcss450/group4/weatherchatapp/MainActivity.java
+++ app/src/main/java/edu/uw/tcss450/group4/weatherchatapp/MainActivity.java
@@ -2,7 +2,11 @@ package edu.uw.tcss450.group4.weatherchatapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +14,33 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.toolbar, menu);
+        return true;
+    }
+
+    @SuppressLint("NonConstantResourceId")
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle item selection
+        switch (item.getItemId()) {
+            case R.id.action_edit_profile:
+                // editProfile();
+                Log.d("EDIT_PROFILE", "Clicked");
+                return true;
+            case R.id.action_settings:
+                // openSettings();
+                Log.d("SETTINGS", "Clicked");
+                return true;
+            case R.id.action_log_out:
+                // logOut();
+                Log.d("LOG_OUT", "Clicked");
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
     }
 }
