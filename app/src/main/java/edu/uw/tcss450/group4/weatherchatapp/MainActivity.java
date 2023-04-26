82c20d8f87ab473d4e17a8c7b5e16603dcbc9e0f
Andrew Nguyen <andrewnguyen841@gmail.com>
2023-04-26 16:00:51 -0700

    add options menu with items "edit profile," "settings," and "log out"

--- app/src/main/java/edu/uw/tcss450/group4/weatherchatapp/MainActivity.java
+++ app/src/main/java/edu/uw/tcss450/group4/weatherchatapp/MainActivity.java
@@ -1,32 +1,46 @@
 package edu.uw.tcss450.group4.weatherchatapp;
 
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.navigation.NavController;
-import androidx.navigation.Navigation;
-import androidx.navigation.ui.AppBarConfiguration;
-import androidx.navigation.ui.NavigationUI;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
-
-import com.google.android.material.bottomnavigation.BottomNavigationView;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
-
-    private AppBarConfiguration mAppBarConfiguration;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
         setContentView(R.layout.activity_main);
-
-        BottomNavigationView navView = findViewById(R.id.nav_view);
-        mAppBarConfiguration = new AppBarConfiguration.Builder(
-                R.id.navigation_home, R.id.navigation_chats)
-                .build();
-        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
-        NavigationUI.setupActionBarWithNavController(this, navController, mAppBarConfiguration);
-        NavigationUI.setupWithNavController(navView, navController);
     }
 
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.toolbar, menu);
+        return true;
+    }
+
+    @SuppressLint("NonConstantResourceId")
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle item selection
+        switch (item.getItemId()) {
+            case R.id.action_edit_profile:
+                // editProfile();
+                Log.d("EDIT_PROFILE", "Clicked");
+                return true;
+            case R.id.action_settings:
+                // openSettings();
+                Log.d("SETTINGS", "Clicked");
+                return true;
+            case R.id.action_log_out:
+                // logOut();
+                Log.d("LOG_OUT", "Clicked");
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
 }
