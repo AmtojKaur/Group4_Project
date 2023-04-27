62d7b5cfeadfe02dfd36ad4013f792565960b3bc
Chloe D <owlgirlpnw@gmail.com>
2023-04-26 19:19:09 -0700

    stub methods

--- app/src/main/java/edu/uw/tcss450/group4/weatherchatapp/ChatFragment.java
+++ app/src/main/java/edu/uw/tcss450/group4/weatherchatapp/ChatFragment.java
@@ -2,7 +2,10 @@ package edu.uw.tcss450.group4.weatherchatapp;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.lifecycle.ViewModelProvider;
 
 import android.view.LayoutInflater;
 import android.view.View;
@@ -14,11 +17,23 @@ import android.view.ViewGroup;
 public class ChatFragment extends Fragment {
 
     private ChatViewModel mViewModel;
+    //private ChatFragmentBinding binding;
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        mViewModel = new ViewModelProvider(getActivity()).get(ChatViewModel.class);
+    }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_chat, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
     }
 }
