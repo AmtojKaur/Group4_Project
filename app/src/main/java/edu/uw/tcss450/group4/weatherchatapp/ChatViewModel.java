99e21bb56fe53da0e018f9d47e5d392befb85d24
Chloe D <owlgirlpnw@gmail.com>
2023-04-26 18:26:35 -0700

    added project specifications comment

--- app/src/main/java/edu/uw/tcss450/group4/weatherchatapp/ChatViewModel.java
+++ app/src/main/java/edu/uw/tcss450/group4/weatherchatapp/ChatViewModel.java
@@ -4,4 +4,14 @@ import androidx.lifecycle.ViewModel;
 
 public class ChatViewModel extends ViewModel {
     // TODO: implement ViewModel
+
+    // Individual or “Group” chat with an existing Connection(s)
+    // Start a chat
+    // Send and receive messages
+    // Messages must be stored (server side)
+    // “See” when the other person is typing (EC)
+    // Store previous messages locally (EC)
+    // Continue an individual chat with an existing Connection
+    // Open a new chat request from an existing Connection
+    // Send/receive Images (EC)
 }
