e8823696b1977a56394d5b4d95bc203429a8c291
Chloe <owlgirlpnw@gmail.com>
2023-04-30 17:32:31 -0700

    initial home to chat navigation

--- app/src/main/java/edu/uw/tcss450/group4/weatherchatapp/MainActivity.java
+++ app/src/main/java/edu/uw/tcss450/group4/weatherchatapp/MainActivity.java
@@ -1,6 +1,10 @@
 package edu.uw.tcss450.group4.weatherchatapp;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.ui.AppBarConfiguration;
+import androidx.navigation.ui.NavigationUI;
 
 import android.annotation.SuppressLint;
 import android.os.Bundle;
@@ -8,12 +12,24 @@ import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
 public class MainActivity extends AppCompatActivity {
+
+    private AppBarConfiguration mAppBarConfiguration;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        BottomNavigationView navView = findViewById(R.id.bottom_nav);
+        mAppBarConfiguration = new AppBarConfiguration.Builder(
+                R.id.navigation_home, R.id.navigation_chat, R.id.navigation_weather)
+                .build();
+        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
+        NavigationUI.setupActionBarWithNavController(this, navController, mAppBarConfiguration);
+        NavigationUI.setupWithNavController(navView, navController);
     }
 
     @Override
@@ -28,15 +44,12 @@ public class MainActivity extends AppCompatActivity {
         // Handle item selection
         switch (item.getItemId()) {
             case R.id.action_edit_profile:
-                // editProfile();
                 Log.d("EDIT_PROFILE", "Clicked");
                 return true;
             case R.id.action_settings:
-                // openSettings();
                 Log.d("SETTINGS", "Clicked");
                 return true;
             case R.id.action_log_out:
-                // logOut();
                 Log.d("LOG_OUT", "Clicked");
                 return true;
             default:
