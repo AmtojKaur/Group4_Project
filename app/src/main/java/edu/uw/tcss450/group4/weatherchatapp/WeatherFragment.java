36d26ed5d62629c7b83c639aafd41888a8ea9ab0
Andrew Nguyen <andrewnguyen841@gmail.com>
2023-05-02 15:54:36 -0700

    push mock home weather fragment

---
+++ app/src/main/java/edu/uw/tcss450/group4/weatherchatapp/WeatherFragment.java
@@ -0,0 +1,36 @@
+package edu.uw.tcss450.group4.weatherchatapp;
+
+import androidx.lifecycle.ViewModelProvider;
+
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class WeatherFragment extends Fragment {
+
+    private WeatherViewModel mViewModel;
+
+    public static WeatherFragment newInstance() {
+        return new WeatherFragment();
+    }
+
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
+                             @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_weather, container, false);
+    }
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+        mViewModel = new ViewModelProvider(this).get(WeatherViewModel.class);
+        // TODO: Use the ViewModel
+    }
+
+}
