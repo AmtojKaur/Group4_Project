04fdcf829c2e97320da86eb739d30ca8b5509ceb
Chloe <owlgirlpnw@gmail.com>
2023-05-02 11:16:41 -0700

    varying dummy chat data

--- app/src/main/java/edu/uw/tcss450/group4/weatherchatapp/chat/ChatGenerator.java
+++ app/src/main/java/edu/uw/tcss450/group4/weatherchatapp/chat/ChatGenerator.java
@@ -3,6 +3,7 @@ package edu.uw.tcss450.group4.weatherchatapp.chat;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.Random;
 
 /**
  * This class is used to create Dummy Chats. Used for development.
@@ -10,30 +11,32 @@ import java.util.List;
 
 public class ChatGenerator {
     private static final ArrayList<ChatPreview> CHATS;
-    public static int COUNT = 5;
+    private static int COUNT = 7;
+
+    private static Random rand;
+
+    private static String[] dummyNames = {"Jane", "John", "Amtoj", "AJ", "Andrew", "Zak", "Chloe"};
+    private static String[] dummyMessage = {"Hi", "This is our first chat", "I'm working on Heroku", "Dummy message", "ok", "hello", "sup"};
+    private static String[] dummyTime = {"9:25 pm", "12:00 pm", "8:06 am", "1:00 am", "Yesterday", "Saturday", "4/25"};
+
 
     static {
         CHATS = new ArrayList<>();
+
         for (int i = 0; i < COUNT; i++) {
             CHATS.add(new ChatPreview
-                    .Builder("Jane", "Hey there, this is John.", "12:00 am")
+                    .Builder(dummyNames[i], dummyMessage[i], dummyTime[i])
                     .build());
         }
-
     }
 
     public static List<ChatPreview> getChatList() {
-        //return Arrays.asList(CHATS);
         return CHATS;
     }
 
 //    public static ChatPreview[] getCHATS() {
 //        return Arrays.copyOf(CHATS, CHATS.length);
 //    }
-
-    public void setCount(int num) {
-        COUNT = num;
-    }
 
     /**
      * Empty private constructor.
