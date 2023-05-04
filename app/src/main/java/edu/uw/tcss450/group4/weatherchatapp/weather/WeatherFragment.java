f376c9e3c135886ceab6444b6ecea2db1a5b8292
Abdirizak Ali <59887662+Abdirizak-byte@users.noreply.github.com>
2023-05-04 08:27:00 -0700

    Update WeatherFragment.java
    Java Doc

--- app/src/main/java/edu/uw/tcss450/group4/weatherchatapp/weather/WeatherFragment.java
+++ app/src/main/java/edu/uw/tcss450/group4/weatherchatapp/weather/WeatherFragment.java
@@ -12,34 +12,32 @@ import android.view.ViewGroup;
 import edu.uw.tcss450.group4.weatherchatapp.R;
 
 /**
- * A simple {@link Fragment} subclass.
- * Use the {@link WeatherFragment#newInstance} factory method to
- * create an instance of this fragment.
+ * Class that prepares and manages data associated with a weather object.
+ *
+ * @author Abdirizak Ali
  */
+ 
 public class WeatherFragment extends Fragment {
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+   
     private static final String ARG_PARAM1 = "param1";
     private static final String ARG_PARAM2 = "param2";
 
-    // TODO: Rename and change types of parameters
     private String mParam1;
     private String mParam2;
-
+    /**
+     * Fragment class that prepares and manages data associated with a weather object.
+     */
     public WeatherFragment() {
         // Required empty public constructor
     }
 
     /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
+     * Use this factory method to create a new instance of the WeatherFragment using the provided parameters.
      * @param param1 Parameter 1.
      * @param param2 Parameter 2.
      * @return A new instance of fragment WeatherFragment.
      */
-    // TODO: Rename and change types and number of parameters
     public static WeatherFragment newInstance(String param1, String param2) {
         WeatherFragment fragment = new WeatherFragment();
         Bundle args = new Bundle();
@@ -48,7 +46,11 @@ public class WeatherFragment extends Fragment {
         fragment.setArguments(args);
         return fragment;
     }
-
+    /**
+     * This method is called when the fragment is created.
+     * It retrieves arguments passed during fragment initialization.
+     * @param savedInstanceState The saved instance state bundle.
+     */
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -57,7 +59,14 @@ public class WeatherFragment extends Fragment {
             mParam2 = getArguments().getString(ARG_PARAM2);
         }
     }
-
+    /**
+     * This method is called when the fragment view is created.
+     * It inflates the layout for this fragment.
+     * @param inflater The layout inflater.
+     * @param container The view group container.
+     * @param savedInstanceState The saved instance state bundle.
+     * @return The inflated view.
+     */
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
