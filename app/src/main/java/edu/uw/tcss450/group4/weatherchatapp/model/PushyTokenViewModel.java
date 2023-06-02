3c8d1454bf3b839e8c237e2001d84327018bcb9b
amtojkaur <amtojk@uw.edu>
2023-06-01 21:24:09 -0700

    Changed PushyTokenViewModel.java so that its no longer for a singular chat and so that it supports multiple. Might still be buggy.

--- app/src/main/java/edu/uw/tcss450/group4/weatherchatapp/model/PushyTokenViewModel.java
+++ app/src/main/java/edu/uw/tcss450/group4/weatherchatapp/model/PushyTokenViewModel.java
@@ -27,7 +27,7 @@ import edu.uw.tcss450.group4.weatherchatapp.R;
 import edu.uw.tcss450.group4.weatherchatapp.io.RequestQueueSingleton;
 import me.pushy.sdk.Pushy;
 
-public class PushyTokenViewModel extends AndroidViewModel{
+/*public class PushyTokenViewModel extends AndroidViewModel{
     private final MutableLiveData<String> mPushyToken;
     private final MutableLiveData<JSONObject> mResponse;
 
@@ -39,11 +39,11 @@ public class PushyTokenViewModel extends AndroidViewModel{
         mResponse.setValue(new JSONObject());
     }
 
-    /**
+    *//**
      * Register as an observer to listen for the PushToken.
      * @param owner the fragments lifecycle owner
      * @param observer the observer
-     */
+     *//*
     public void addTokenObserver(@NonNull LifecycleOwner owner,
                                  @NonNull Observer<? super String> observer) {
         mPushyToken.observe(owner, observer);
@@ -67,11 +67,11 @@ public class PushyTokenViewModel extends AndroidViewModel{
         }
     }
 
-    /**
+    *//**
      * This is the method described in the Pushy documentation. Note the Android class
      * AsyncTask is deprecated as of Android Q. It is fine to use here and for this
      * quarter. In your future Android development, look for an alternative solution.
-     */
+     *//*
     private class RegisterForPushNotificationsAsync extends AsyncTask<Void, Void, String> {
         protected String doInBackground(Void... params) {
             String deviceToken;
@@ -98,11 +98,11 @@ public class PushyTokenViewModel extends AndroidViewModel{
         }
     }
 
-    /**
+    *//**
      * Send this Pushy device token to the web service.
-     * @param jwt
+     * @param
      * @throws IllegalStateException when this method is called before the token is retrieve
-     */
+     *//*
     public void sendTokenToWebservice(final String jwt) {
         if (mPushyToken.getValue().isEmpty()) {
             throw new IllegalStateException("No pushy token. Do NOT call until token is retrieved");
@@ -168,4 +168,146 @@ public class PushyTokenViewModel extends AndroidViewModel{
             }
         }
     }
+}*/
+
+public class PushyTokenViewModel extends AndroidViewModel {
+
+    private final MutableLiveData<String> mPushyToken;
+    private final MutableLiveData<JSONObject> mResponse;
+
+    public PushyTokenViewModel(@NonNull Application application) {
+        super(application);
+        mPushyToken = new MutableLiveData<>();
+        mPushyToken.setValue("");
+        mResponse = new MutableLiveData<>();
+        mResponse.setValue(new JSONObject());
+    }
+
+    /**
+     * Register as an observer to listen for the PushToken.
+     * @param owner the fragments lifecycle owner
+     * @param observer the observer
+     */
+    public void addTokenObserver(@NonNull LifecycleOwner owner,
+                                 @NonNull Observer<? super String> observer) {
+        mPushyToken.observe(owner, observer);
+    }
+
+    public void addResponseObserver(@NonNull LifecycleOwner owner,
+                                    @NonNull Observer<? super JSONObject> observer) {
+        mResponse.observe(owner, observer);
+    }
+
+    public void retrieveToken() {
+        if (!Pushy.isRegistered(getApplication().getApplicationContext())) {
+
+            Log.d("PUSH VIEW MODEL", "FETCHING NEW TOKEN");
+            new RegisterForPushNotificationsAsync().execute();
+
+        } else {
+            Log.d("PUSH VIEW MODEL", "USING OLD TOKEN");
+            mPushyToken.setValue(
+                    Pushy.getDeviceCredentials(getApplication().getApplicationContext()).token);
+        }
+    }
+
+    /**
+     * This is the method described in the Pushy documentation. Note the Android class
+     * AsyncTask is deprecated as of Android Q. It is fine to use here and for this
+     * quarter. In your future Android development, look for an alternative solution.
+     */
+    private class RegisterForPushNotificationsAsync extends AsyncTask<Void, Void, String> {
+        protected String doInBackground(Void... params) {
+            String deviceToken;
+            try {
+                // Assign a unique token to this device
+                deviceToken = Pushy.register(getApplication().getApplicationContext());
+            }
+            catch (Exception e) {
+                // Return exc to onPostExecute
+                return e.getMessage();
+            }
+            // Success
+            return deviceToken;
+        }
+
+        @Override
+        protected void onPostExecute(String token) {
+            if (token.isEmpty()) {
+                // Show error in log - You should add error handling for the user.
+                Log.e("ERROR RETRIEVING PUSHY TOKEN", token);
+            } else {
+                mPushyToken.setValue(token);
+            }
+        }
+    }
+
+    /**
+     * Send this Pushy device token to the web service.
+     * @param jwt
+     * @throws IllegalStateException when this method is called before the token is retrieve
+     */
+    public void sendTokenToWebservice(final String jwt) {
+        if (mPushyToken.getValue().isEmpty()) {
+            throw new IllegalStateException("No pushy token. Do NOT call until token is retrieved");
+        }
+
+        String url = "https://amtojk-tcss450-labs.herokuapp.com/" +
+                "auth";
+
+        JSONObject body = new JSONObject();
+        try {
+            body.put("token", mPushyToken.getValue());
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+
+        Request request = new JsonObjectRequest(
+                Request.Method.PUT,
+                url,
+                body, //push token found in the JSONObject body
+                mResponse::setValue,
+                this::handleError) {
+
+            @Override
+            public Map<String, String> getHeaders() {
+                Map<String, String> headers = new HashMap<>();
+                // add headers <key,value>
+                headers.put("Authorization", jwt);
+                return headers;
+            }
+        };
+
+        request.setRetryPolicy(new DefaultRetryPolicy(
+                10_000,
+                DefaultRetryPolicy.DEFAULT_MAX_RETRIES,
+                DefaultRetryPolicy.DEFAULT_BACKOFF_MULT));
+        //Instantiate the RequestQueue and add the request to the queue
+        RequestQueueSingleton.getInstance(getApplication().getApplicationContext())
+                .addToRequestQueue(request);
+    }
+
+    private void handleError(final VolleyError error) {
+        if (Objects.isNull(error.networkResponse)) {
+            try {
+                mResponse.setValue(new JSONObject("{" +
+                        "error:\"" + error.getMessage() +
+                        "\"}"));
+            } catch (JSONException e) {
+                Log.e("JSON PARSE", "JSON Parse Error in handleError");
+            }
+        }
+        else {
+            String data = new String(error.networkResponse.data, Charset.defaultCharset());
+            try {
+                mResponse.setValue(new JSONObject("{" +
+                        "code:" + error.networkResponse.statusCode +
+                        ", data:" + data +
+                        "}"));
+            } catch (JSONException e) {
+                Log.e("JSON PARSE", "JSON Parse Error in handleError");
+            }
+        }
+    }
 }
+
