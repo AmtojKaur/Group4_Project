dc59ceae2cf0645026b7d20e6b99f0aa7f72e1ab
amtojkaur <amtojk@uw.edu>
2023-06-04 18:40:19 -0700

    Chat Functionality: Not Done

--- app/src/main/java/edu/uw/tcss450/group4/weatherchatapp/ui/chat/ChatNewChatViewModel.java
+++ app/src/main/java/edu/uw/tcss450/group4/weatherchatapp/ui/chat/ChatNewChatViewModel.java
@@ -8,14 +8,23 @@ import androidx.lifecycle.LifecycleOwner;
 import androidx.lifecycle.MutableLiveData;
 import androidx.lifecycle.Observer;
 
+import com.android.volley.DefaultRetryPolicy;
+import com.android.volley.Request;
+import com.android.volley.toolbox.JsonObjectRequest;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import edu.uw.tcss450.group4.weatherchatapp.ui.connections.UserObject;
 
 public class ChatNewChatViewModel extends AndroidViewModel {
 
-    private final MutableLiveData<List<UserObject>> mUserList;
+   private MutableLiveData<JSONObject> mResponse;
     /**
      * Public constructor to declare a new MutableLiveData object and
      * sets its value to a List.
@@ -24,8 +33,8 @@ public class ChatNewChatViewModel extends AndroidViewModel {
      */
     public ChatNewChatViewModel(@NonNull Application application) {
         super(application);
-        mUserList = new MutableLiveData<>();
-        mUserList.setValue(new ArrayList<>());
+        mResponse = new MutableLiveData<>();
+        mResponse.setValue(new JSONObject());
     }
 
     /**
@@ -34,7 +43,41 @@ public class ChatNewChatViewModel extends AndroidViewModel {
      * @param observer the live data associated with the list of UserObjects objects
      */
     public void addChatNewChatObserver(@NonNull LifecycleOwner owner,
-                                           @NonNull Observer<? super List<UserObject>> observer) {
-        mUserList.observe(owner, observer);
+                                           @NonNull Observer<? super JSONObject> observer) {
+        mResponse.observe(owner, observer);
+    }
+
+    public void createChat(final String jwt, final String chatName) {
+        String url = "https://amtojk-tcss450-labs.herokuapp.com/chats";
+        JSONObject body = new JSONObject();
+        try{
+            body.put("name", chatName);
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+
+        Request request = new JsonObjectRequest(
+                Request.Method.POST,
+                url,
+                body,
+                mResponse::setValue,
+                this::handleError) {
+            @Override
+            public Map<String, String> getHeaders() {
+                Map<String, String> headers = new HashMap<>();
+                // add headers <key,value>
+                headers.put("Authorization", jwt);
+                return headers;
+            }
+        };
+
+        request.setRetryPolicy(new DefaultRetryPolicy(
+                10_000,
+                DefaultRetryPolicy.DEFAULT_MAX_RETRIES,
+                DefaultRetryPolicy.DEFAULT_BACKOFF_MULT));
+        //Instantiate the RequestQueue and add the request to the queue
+        RequestQueueSingleton.getInstance(getApplication().getApplicationContext())
+                .addToRequestQueue(request);
+
     }
 }
